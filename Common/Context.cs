40eb99aca35ec000b0b4708bd10b0667b2143869
fkarahan <sfatihkarahan@gmail.com>
2019-12-08 22:03:28 +0300

    dal and rabbitmq fix

---
+++ Common/Context.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Common
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Queue> Queues { get; set; }
+    }
+}
---
+++ Service/Queue/QueueService.cs
@@ -0,0 +1,24 @@
+﻿namespace Service.Queue
+{
+    using Common;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Data.Entities;
+    using System.Linq;
+
+    public class QueueService : IQueueService
+    {
+        private readonly Context _dbContext;
+        public QueueService(Context dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+        public void AddQueue(Queue queue) { _dbContext.Queues.Add(queue); _dbContext.SaveChanges(); }
+
+        public List<Queue> GetQueue(string QueueName)
+        {
+           return _dbContext.Queues.Where(x => x.QueueName == QueueName).ToList();
+        }
+    }
+}
