44cc8bb3493c02715b35c6293b204e7daa4a2009
fkarahan <sfatihkarahan@gmail.com>
2019-12-15 01:51:53 +0300

    connection added

---
+++ ES/ESManager.cs
@@ -0,0 +1,35 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ES
+{
+    public class ESManager 
+    {
+        private static ElasticClient _client = null;
+        public static ElasticClient Client
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    lock (new Object())
+                    {
+                        if (_client == null)
+                        {
+                            List<Uri> nodes = new List<Uri>();
+                            
+                                nodes.Add(new Uri("http://localhost:9200"));
+                            var pool = new SniffingConnectionPool(nodes);
+                            ConnectionSettings settings = null;
+                            _client = new ElasticClient(settings);
+                        }
+                    }
+                }
+                return _client;
+            }
+        }
+    }
+}
---
+++ ES/SearchManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ES
+{
+    public class SearchManager : ESManager
+    {
+    }
+}
